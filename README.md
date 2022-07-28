e33d5acc4d5da1988f0f6b62777b7f642af7a662
Tianrui Li <ltianrui@ltianrui-a01.vmware.com>
2022-07-28 14:21:28 -0700

    SQLAlchemy

---
+++ README.md
@@ -0,0 +1,6 @@
+# Borathon
+
+## Installation
+
+1. Navigate to your desired conda environment.
+2. Run `conda install --file requirements.txt`
--- Transaction.py
+++ Transaction.py
@@ -1,4 +1,16 @@
-class Transaction:
+from sqlalchemy import Column, Integer, Numeric
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    id = Column(Integer, primary_key=True)
+    account = Column(Integer)
+    type = Column(Integer)
+    associated_account = Column(Integer)
+
     def __init__(self, id, amount, type, associated_account):
         self.id = id
         self.amount = amount
