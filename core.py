8c70980ddb598a875f7cd5a49cecce563f3586b0
rycorkery <rycorkery@outlook.com>
2022-07-28 15:27:58 -0400

    Update core.py

--- core.py
+++ core.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request, abort
+from CustomerAccount import CustomerAccount
+import json
 
 app = Flask(__name__)
 customerAccounts = []
@@ -6,7 +8,14 @@ transactions = []
 
 @app.route("/api/CustomerAccount/GetCustomerAccountByAccountNumber", methods = ["GET"])
 def lookupAccount():
-    return
+    id = int(request.args.get('id'))
+    if id == None:
+        abort(400, 'Insuffient ID specified.')
+    for customerAccount in customerAccounts:
+        print(customerAccount.id)
+        if customerAccount.id == id:
+            return json.dumps(customerAccount.__dict__)
+    abort(400, "This ID was not found.")
 
 @app.route("/api/CustomerAccount/OpenCustomerAccount", methods = ["POST"])
 def openAccount():
@@ -19,3 +28,6 @@ def closeAccount():
 @app.route("/api/CustomerAccount/ApplyTransactionToCustomerAccount", methods = ["GET"])
 def applyTransaction():
     return
+
+if __name__ == "__main__":
+    app.run()
